64bd46b1d2cc514de6a49a10cb4424aeeb846d8b
unknown <you@example.com>
2021-05-29 10:09:42 +0300

    made some modification to the bar graph

--- app/src/main/java/com/example/VisualAnalysis/MainActivity.java
+++ app/src/main/java/com/example/VisualAnalysis/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.VisualAnalysis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
@@ -18,8 +21,17 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<BarEntry> dataVals = new ArrayList<BarEntry>();
         dataVals.add(new BarEntry(0, 3));
         dataVals.add(new BarEntry(1, 4));
+        dataVals.add(new BarEntry(2, 9));
         dataVals.add(new BarEntry(3, 6));
         dataVals.add(new BarEntry(4, 10));
+        dataVals.add(new BarEntry(5, 12));
+        dataVals.add(new BarEntry(6, 15));
+        dataVals.add(new BarEntry(7, 8));
+        dataVals.add(new BarEntry(8, 10));
+        dataVals.add(new BarEntry(9, 9));
+        dataVals.add(new BarEntry(10, 12));
+        dataVals.add(new BarEntry(0, -3));
+        dataVals.add(new BarEntry(5, -5));
 
         return dataVals;
     }
@@ -32,9 +44,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BarChart barChart = findViewById(R.id.barchart);
-        BarDataSet barDataSet = new BarDataSet(dataValues(),"dataset1");
+        BarDataSet barDataSet = new BarDataSet(dataValues(), "dataset1");
+        barDataSet.setColor(Color.parseColor("#212c5d"));
+        barDataSet.setDrawValues(false);
 
-        BarData barData= new BarData();
+        //barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.setDrawGridBackground(false);
+        barChart.getAxisLeft().setDrawGridLines(false);
+        barChart.getAxisRight().setDrawGridLines(false);
+        XAxis xAxis=barChart.getXAxis();
+        xAxis.setDrawGridLines(false);
+
+
+
+        //baChart.setDrawGridBackground(false);
+        barDataSet.setBarBorderWidth(1f);
+
+        BarData barData = new BarData();
         barData.addDataSet(barDataSet);
         barChart.setData(barData);
         barChart.invalidate();
