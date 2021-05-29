5c69b9d6eb3901c4679ccac98360309264a4715e
unknown <you@example.com>
2021-05-29 10:49:21 +0300

    made the barchart a combination of few colors

--- app/src/main/java/com/example/VisualAnalysis/MainActivity.java
+++ app/src/main/java/com/example/VisualAnalysis/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.components.XAxis;
@@ -16,8 +17,28 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    int negativeColor = Color.rgb(17, 15, 71);
+    int positiveColor = Color.rgb(217, 245, 255);
 
-    ArrayList<BarEntry> dataValues() {
+
+//        for(
+//    int i = 0;i<dataVals.size();i++)
+//
+//    {
+//        BarEntry d = dataVals.get(i);
+//
+//        if (d.getY() <= 0) {
+//
+//        }
+
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
         ArrayList<BarEntry> dataVals = new ArrayList<BarEntry>();
         dataVals.add(new BarEntry(0, 3));
         dataVals.add(new BarEntry(1, 4));
@@ -30,35 +51,34 @@ public class MainActivity extends AppCompatActivity {
         dataVals.add(new BarEntry(8, 10));
         dataVals.add(new BarEntry(9, 9));
         dataVals.add(new BarEntry(10, 12));
-        dataVals.add(new BarEntry(0, -3));
+        dataVals.add(new BarEntry(11, -3));
         dataVals.add(new BarEntry(5, -5));
 
-        return dataVals;
-    }
-
-
-    @Override
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         BarChart barChart = findViewById(R.id.barchart);
-        BarDataSet barDataSet = new BarDataSet(dataValues(), "dataset1");
-        barDataSet.setColor(Color.parseColor("#212c5d"));
+        BarDataSet barDataSet = new BarDataSet(dataVals, "dataset1");
+
         barDataSet.setDrawValues(false);
+        for (int i = 0; i < dataVals.size(); i++) {
+            if (dataVals.get(i).getY() < 0) {
+                barDataSet.setColors(negativeColor,positiveColor);
+            } else if(dataVals.get(i).getY()>0) {
+                barDataSet.setColors(positiveColor,negativeColor);
+            }
+
+        }
 
         //barChart.getAxisLeft().setDrawGridLines(false);
         barChart.setDrawGridBackground(false);
         barChart.getAxisLeft().setDrawGridLines(false);
         barChart.getAxisRight().setDrawGridLines(false);
-        XAxis xAxis=barChart.getXAxis();
+        XAxis xAxis = barChart.getXAxis();
         xAxis.setDrawGridLines(false);
-
 
 
         //baChart.setDrawGridBackground(false);
         barDataSet.setBarBorderWidth(1f);
+
 
         BarData barData = new BarData();
         barData.addDataSet(barDataSet);
