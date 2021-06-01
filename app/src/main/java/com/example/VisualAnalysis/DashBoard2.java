8007cae1e2b49018bcf4eab79c2474154398d2b7
unknown <you@example.com>
2021-06-01 17:28:23 +0300

    it has gone crazy, i mean the second line chart values showing up on the first

--- app/src/main/java/com/example/VisualAnalysis/DashBoard2.java
+++ app/src/main/java/com/example/VisualAnalysis/DashBoard2.java
@@ -46,6 +46,10 @@ public class DashBoard2 extends AppCompatActivity {
         lineChart.getDescription().setEnabled(false);
         lineChart.getXAxis().setDrawAxisLine(false);
         lineChart.getXAxis().setDrawGridLines(false);
+//        lineChart.getXAxis().enableAxisLineDashedLine(10f,10f,0);
+//        lineChart.getAxisRight().enableAxisLineDashedLine(10f,10f,0);
+//        lineChart.getAxisLeft().enableAxisLineDashedLine(10f,10f,0);
+
         lineChart.getXAxis().setPosition(XAxis.XAxisPosition.BOTTOM);
         lineChart.setDrawGridBackground(false);
         lineChart.getAxisLeft().setDrawGridLines(false);
@@ -55,13 +59,43 @@ public class DashBoard2 extends AppCompatActivity {
         lineDataSet.setDrawValues(false);
         lineDataSet.setColors(Color.parseColor("#5b79e7"));
         lineDataSet.setDrawCircles(false);
+//        lineDataSet.enableDashedLine(10f,10f,0);
 
 
         ArrayList<String> xAxisVals = new ArrayList<>(Arrays.asList("Apr 6", "Apr 7", "Apr 8", "Apr 9", "Apr 10", "Apr 11", "Apr 12"));
         ArrayList<String> yAxisVals = new ArrayList<>(Arrays.asList("0", "10k", "20k", "30k", "40k", "50k", "60k"));
         lineChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(xAxisVals));
-        lineChart.getAxisLeft().setValueFormatter(new IndexAxisValueFormatter(yAxisVals));
+        //lineChart.getAxisLeft().setValueFormatter(new IndexAxisValueFormatter(yAxisVals));
         lineChart.getAxisLeft().setLabelCount(yAxisVals.size());
+
+        LineChart lineChart2 = findViewById(R.id.linechart2);
+        ArrayList<Entry> dataVal1 = new ArrayList<Entry>();
+        dataVals.add(new Entry(0, 10f));
+        dataVals.add(new Entry(1, 2));
+        dataVals.add(new Entry(2, 10f));
+        dataVals.add(new Entry(3, 6f));
+        dataVals.add(new Entry(4, 15f));
+        dataVals.add(new Entry(5, 4f));
+        dataVals.add(new Entry(6, 8f));
+
+        ArrayList<Entry> dataVal2 = new ArrayList<Entry>();
+        dataVals.add(new Entry(0, 5f));
+        dataVals.add(new Entry(1, 18f));
+        dataVals.add(new Entry(2, 11f));
+        dataVals.add(new Entry(3, 14f));
+        dataVals.add(new Entry(4, 4f));
+        dataVals.add(new Entry(5, 16f));
+        dataVals.add(new Entry(6, 18f));
+
+        LineDataSet lineDataSet1 = new LineDataSet(dataVal1, "Install");
+        LineDataSet lineDataSet2 = new LineDataSet(dataVal2, "Uninstall");
+
+        lineDataSet1.setColors(Color.parseColor("#27adb9"));
+        lineDataSet2.setColors(Color.parseColor("#5473e8"));
+
+        LineData lineData2 = new LineData();
+        lineData2.addDataSet(lineDataSet1);
+        lineChart2.setData(lineData2);
 
 
 
