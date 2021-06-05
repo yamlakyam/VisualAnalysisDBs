3bd4ecb5ee33b3f30597805952390b9ec6cce142
unknown <yamlakyam@gmail.com>
2021-06-05 11:54:42 +0300

    the google map looks a little better now, zoomed in and multiple markers added on it

--- app/src/main/java/com/example/VisualAnalysis/MapActivity.java
+++ app/src/main/java/com/example/VisualAnalysis/MapActivity.java
@@ -11,6 +11,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
 public class MapActivity extends AppCompatActivity {
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
@@ -26,9 +28,32 @@ public class MapActivity extends AppCompatActivity {
          */
         @Override
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+            LatLng loc1 = new LatLng(9.016947,38.764635);
+            LatLng loc2 = new LatLng(9.016677,38.766920);
+            LatLng loc3 = new LatLng(9.016210,38.770046);
+            LatLng loc4 = new LatLng(9.017683,38.770271);
+            LatLng loc5 = new LatLng(9.007278,38.776499);
+
+            ArrayList<LatLng> locations = new ArrayList<LatLng>();
+            locations.add(loc1);
+            locations.add(loc2);
+            locations.add(loc3);
+            locations.add(loc4);
+            locations.add(loc5);
+
+            for(LatLng i:locations){
+                googleMap.addMarker(new MarkerOptions().position(i).title("location "+locations.indexOf(i)));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(i));
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(i.latitude,i.longitude),12.0f));
+            }
+
+
+//            LatLng kazanchis = new LatLng(9.015599,38.771664);
+//
+//            googleMap.addMarker(new MarkerOptions().position(kazanchis).title("Marker in Sydney"));
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(kazanchis));
+
         }
     };
 
