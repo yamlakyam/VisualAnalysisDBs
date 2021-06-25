87f99f58d2df77133ae9b3d1b81728893f05861f
unknown <yamlakyam@gmail.com>
2021-06-25 17:41:31 +0300

    things are pretty much messed up after trying to inflate the rows from another layout

--- app/src/main/java/com/example/VisualAnalysis/DashBoard3Fragment.java
+++ app/src/main/java/com/example/VisualAnalysis/DashBoard3Fragment.java
@@ -42,6 +42,18 @@ public class DashBoard3Fragment extends Fragment {
     static int widthDp;
     static int heightDp;
 
+    private static ImageView imageView;
+    private static TextView textView1;
+    private static TextView textView2;
+    private static TextView textView3;
+    private static TextView textView4;
+    private static TextView textView5;
+    private static TextView textView6;
+    private static TableRow tableRow;
+    private static LinearLayout linearLayout;
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -50,9 +62,27 @@ public class DashBoard3Fragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_dash_board3, container, false);
 
+        LinearLayout tableElements =(LinearLayout) getLayoutInflater().inflate(R.layout.table_elements,null,false);
 
-        tableLayout = view.findViewById(R.id.table);
-        scrollView = view.findViewById(R.id.scrollView);
+        tableLayout = view.findViewById(R.id.tableLayout);
+//        textView1 = view.findViewById(R.id.tvalue1);
+//        textView1=(TextView)getLayoutInflater().inflate(R.id.tvalue1,null);
+       textView1=tableElements.findViewById(R.id.tvalue1);
+       textView2=tableElements.findViewById(R.id.tvalue2);
+       textView3=tableElements.findViewById(R.id.tvalue3);
+       textView4=tableElements.findViewById(R.id.tvalue4);
+       textView5=tableElements.findViewById(R.id.tvalue5);
+       textView6=tableElements.findViewById(R.id.tvalue6);
+       tableRow=tableElements.findViewById(R.id.tableRow);
+       scrollView=view.findViewById(R.id.scrolll);
+       linearLayout=tableElements.findViewById(R.id.tableElementLayout);
+
+
+//        linearLayout = view.findViewById(R.id.linearLayout1);
+
+
+
+
 
 
         //conversion to dp
@@ -111,24 +141,25 @@ public class DashBoard3Fragment extends Fragment {
                     requireActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-                            TextView txtv1 = new TextView(getContext());
-                            TextView txtv2 = new TextView(getContext());
-                            TextView txtv3 = new TextView(getContext());
-                            TextView txtv4 = new TextView(getContext());
-                            TextView txtv5 = new TextView(getContext());
-                            TextView txtv6 = new TextView(getContext());
-                            txtv1.setText(DashBoard3Fragment.tables.get(finalI).branchName);
-                            txtv2.setText(DashBoard3Fragment.tables.get(finalI).percentage);
-                            txtv3.setText(DashBoard3Fragment.tables.get(finalI).total);
+//                            TextView txtv1 = new TextView(getContext());
+//                            TextView txtv2 = new TextView(getContext());
+//                            TextView txtv3 = new TextView(getContext());
+//                            TextView txtv4 = new TextView(getContext());
+//                            TextView txtv5 = new TextView(getContext());
+//                            TextView txtv6 = new TextView(getContext());
+
+                            textView1.setText(DashBoard3Fragment.tables.get(finalI).branchName);
+                            textView2.setText(DashBoard3Fragment.tables.get(finalI).percentage);
+                            textView3.setText(DashBoard3Fragment.tables.get(finalI).total);
 
                             String date = java.text.DateFormat.getDateInstance().format(DashBoard3Fragment.tables.get(finalI).date);
 //                            String date = new SimpleDateFormat("yyyy-mm-dd").format(DashBoard3Fragment.tables.get(finalI).date);
 
-                            txtv4.setText(date);
-                            txtv5.setText(DashBoard3Fragment.tables.get(finalI).day);
-                            txtv6.setText(DashBoard3Fragment.tables.get(finalI).lastSeen);
+                            textView4.setText(date);
+                            textView5.setText(DashBoard3Fragment.tables.get(finalI).day);
+                            textView6.setText(DashBoard3Fragment.tables.get(finalI).lastSeen);
                             //            initTable(tables.get(i).tableRow, new TextView(this).setText(tables.get(i).branchName),new TextView(this).setText(tables.get(i).percentage),new TextView(this).setText(tables.get(i).total));
-                            DashBoard3Fragment.initTable(DashBoard3Fragment.tables.get(finalI).tableRow, txtv1, txtv2, txtv3, txtv4, txtv5, txtv6, getContext());
+                            DashBoard3Fragment.initTable( getContext());
                             Log.i("TAG", tables.get(finalI).branchName);
                         }
                     });
@@ -185,7 +216,7 @@ public class DashBoard3Fragment extends Fragment {
         long elapsedSeconds = difference / secondsInMilli;
 
         if (elapsedMonths > 12) {
-            return " years ago";
+            return " long ago";
         } else if (elapsedMonths <= 12 && elapsedMonths > 0) {
             return elapsedMonths + " months ago";
         } else if (elapsedMonths == 0 && elapsedDays > 0) {
@@ -198,79 +229,97 @@ public class DashBoard3Fragment extends Fragment {
             return elapsedSeconds + " seconds ago";
         } else
 //            return elapsedDays + " days, " + elapsedHours + " hours, " + elapsedMinutes + " minutes, " + elapsedSeconds + " seconds ago";
-
             return elapsedSeconds + "";
     }
 
     @SuppressLint("UseCompatLoadingForDrawables")
-    static void initTable(TableRow tr, TextView tV1, TextView tV2, TextView tV3, TextView tV4, TextView tV5, TextView tV6, Context context) {
+    static void initTable( Context context) {
+
+//        tableRow.addView(linearLayout);
+
+//        tableRow.addView(textView1);
+//        tableRow.addView(textView2);
+//        tableRow.addView(textView3);
+//        tableRow.addView(textView4);
+//        tableRow.addView(textView5);
+//        tableRow.addView(textView6);
+//        tableRow.removeAllViews();
+//        tableRow.addView(tableLayout);
 
 
-        tr.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-        tr.setBackgroundColor(Color.parseColor("#49515c"));
-        tV1.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
-        tV2.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
-        tV3.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
-        tV4.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
-        tV5.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
-        tV6.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
-        tV2.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
-        tV3.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
-        tV4.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
-        tV5.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
-        tV6.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+//        tr.setLayoutParams(new TableLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//        tr.setBackgroundColor(Color.parseColor("#49515c"));
+//        tV1.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
+//        tV2.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
+//        tV3.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
+//        tV4.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
+//        tV5.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
+//        tV6.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
+//        tV2.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+//        tV3.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+//        tV4.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+//        tV5.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
+//        tV6.setGravity(Gravity.END | Gravity.CENTER_VERTICAL);
 //        tV3.setPadding(0, 0, paddingPixel, 0);
-        tV5.setPadding(0, 0, paddingPixel, 0);
-        tV1.setPadding(paddingPixel, 0, 0, 0);
-        tV1.setTextColor(Color.parseColor("#d2b566"));
-        if (Double.parseDouble((tV2.getText().toString()).substring(0, tV2.getText().toString().length() - 1)) < 50) {
-            tV2.setTextColor(Color.parseColor("#EA6F6A"));
+//        tV5.setPadding(0, 0, paddingPixel, 0);
+//        tV6.setPadding(0, 0, paddingPixel, 0);
+//        tV1.setPadding(paddingPixel, 0, 0, 0);
+//        tV1.setTextColor(Color.parseColor("#d2b566"));
+
+        if (Double.parseDouble((textView2.getText().toString()).substring(0, textView2.getText().toString().length() - 1)) < 50) {
+            textView2.setTextColor(Color.parseColor("#EA6F6A"));
         } else
-            tV2.setTextColor(Color.parseColor("#78FA5A"));
+            textView2.setTextColor(Color.parseColor("#78FA5A"));
 
 //        Log.i("TAG",tV2.getText().toString());
 //            tV2.setTextColor(Color.parseColor("#78FA5A"));
 
-        tV3.setTextColor(Color.parseColor("#78FA5A"));
-        tV4.setTextColor(Color.parseColor("#78FA5A"));
-        tV5.setTextColor(Color.parseColor("#78FA5A"));
-        tV5.setTextColor(Color.parseColor("#78FA5A"));
-        tV6.setTextColor(Color.parseColor("#78FA5A"));
+//        tV3.setTextColor(Color.parseColor("#78FA5A"));
+//        tV4.setTextColor(Color.parseColor("#78FA5A"));
+//        tV5.setTextColor(Color.parseColor("#78FA5A"));
+//        tV5.setTextColor(Color.parseColor("#78FA5A"));
+//        tV6.setTextColor(Color.parseColor("#78FA5A"));
+//
+//        tV1.setTextSize(20);
+//        tV2.setTextSize(20);
+//        tV3.setTextSize(20);
+//        tV4.setTextSize(20);
+//        tV5.setTextSize(20);
+//        tV6.setTextSize(20);
 
-        tV1.setTextSize(20);
-        tV2.setTextSize(20);
-        tV3.setTextSize(20);
-        tV4.setTextSize(20);
-        tV5.setTextSize(20);
-        tV6.setTextSize(20);
-
-        ImageView imageView = new ImageView(tV1.getContext());
+        ImageView imageView = new ImageView(textView1.getContext());
         Drawable drawable = context.getResources().getDrawable(R.drawable.ic_heineken);
         imageView.setImageDrawable(drawable);
 
         LinearLayout.LayoutParams ll = new LinearLayout.LayoutParams(widthDp, heightDp);
         imageView.setLayoutParams(ll);
-        tV1.setGravity(Gravity.CENTER_VERTICAL);
+//        tV1.setGravity(Gravity.CENTER_VERTICAL);
 
-
-        LinearLayout linearLayout = new LinearLayout(tV1.getContext());
-        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
-        linearLayout.addView(imageView);
-        linearLayout.addView(tV1);
-        linearLayout.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
+//
+//        LinearLayout linearLayout = new LinearLayout(tV1.getContext());
+//        linearLayout.setOrientation(LinearLayout.HORIZONTAL);
+//        linearLayout.addView(imageView);
+//        linearLayout.addView(tV1);
+//        linearLayout.setLayoutParams(new TableRow.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT, 1f));
 //        linearLayout.setGravity(Gravity.CENTER|Gravity.CENTER_VERTICAL);
 
-        tr.addView(linearLayout);
-//        tr.addView(tV1);
-        tr.addView(tV2);
-        tr.addView(tV3);
-        tr.addView(tV4);
-        tr.addView(tV5);
-        tr.addView(tV6);
+//        tr.addView(linearLayout);
+////        tr.addView(tV1);
+//        tr.addView(tV2);
+//        tr.addView(tV3);
+//        tr.addView(tV4);
+//        tr.addView(tV5);
+//        tr.addView(tV6);
 
-        tableLayout.addView(tr);
+//        ((ViewGroup)tableRow.getParent()).removeView(tableRow);
+//        ((ViewGroup)linearLayout.getParent()).removeView(linearLayout);
+
+        if(linearLayout.getParent()!=null)
+            ((ViewGroup)linearLayout.getParent()).removeView(linearLayout);
+
+        tableLayout.addView(linearLayout);
         Animation animation1 = AnimationUtils.loadAnimation(tableLayout.getContext(), R.anim.slide_in_bottom);
         Animation animation2 = AnimationUtils.loadAnimation(tableLayout.getContext(), R.anim.slide_out_bottom);
-        tr.startAnimation(animation2);
+        linearLayout.startAnimation(animation2);
     }
 }
