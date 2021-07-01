54c3f9f93071a0b2cf7332d02049691057927466
unknown <yamlakyam@gmail.com>
2021-07-01 17:04:39 +0300

    the gridview scrols with a delay

--- app/src/main/java/com/example/VisualAnalysis/VsmCardFragment.java
+++ app/src/main/java/com/example/VisualAnalysis/VsmCardFragment.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridView;
-import android.widget.ScrollView;
 
 import androidx.fragment.app.Fragment;
 
@@ -32,14 +31,40 @@ public class VsmCardFragment extends Fragment {
         vsmCardList.add(new Table("VAN4", 15, "15 mins ago", 15, 20098.57));
         vsmCardList.add(new Table("VAN5", 7, "25 mins ago", 26, 95098.68));
         vsmCardList.add(new Table("VAN6", 8, "12 mins ago", 10, 208698.97));
+        vsmCardList.add(new Table("VAN7", 8, "5 mins ago", 10, 208698.97));
+        vsmCardList.add(new Table("VAN8", 8, "2 hrs ago", 10, 208698.97));
+        vsmCardList.add(new Table("VAN9", 8, "4 hrs ago", 10, 208698.97));
+        vsmCardList.add(new Table("VAN10", 8, "5 hrs ago", 10, 208698.97));
 
         VSMcardGVAdapter adapter = new VSMcardGVAdapter(requireContext(), vsmCardList);
         VSMcardGridView.setAdapter(adapter);
 
+        Thread gThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < vsmCardList.size(); i++) {
+                    int finalI = i;
+                    requireActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            VSMcardGridView.smoothScrollToPosition(finalI );
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
 
+                }
+
+            }
+        });
+
+        gThread.start();
         return view;
 
     }
-
-
 }
+
+
